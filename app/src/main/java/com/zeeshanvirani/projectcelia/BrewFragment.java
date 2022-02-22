10d8d7eb391140cad34531fa9b5046aeda16260e
zeeshanvirani <zvirani99@gmail.com>
2022-02-21 18:34:46 -0600

    bottom navigation implemented

---
+++ app/src/main/java/com/zeeshanvirani/projectcelia/BrewFragment.java
@@ -0,0 +1,23 @@
+package com.zeeshanvirani.projectcelia;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BrewFragment extends Fragment {
+
+    public BrewFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_brew, container, false);
+    }
+}
--- app/src/main/java/com/zeeshanvirani/projectcelia/MainActivity.java
+++ app/src/main/java/com/zeeshanvirani/projectcelia/MainActivity.java
@@ -1,8 +1,15 @@
 package com.zeeshanvirani.projectcelia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HistoryFragment historyFragment = new HistoryFragment();
+        BrewFragment brewFragment = new BrewFragment();
+        SettingsFragment settingsFragment = new SettingsFragment();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener( new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch ( item.getItemId() ) {
+                    case R.id.history:
+                        getSupportFragmentManager().beginTransaction()
+                                .setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
+                                .replace( R.id.fragment_container, historyFragment ).commit();
+                        break;
+                    case R.id.brew:
+                        getSupportFragmentManager().beginTransaction()
+                                .setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
+                                .replace( R.id.fragment_container, brewFragment ).commit();
+                        break;
+                    case R.id.settings:
+                        getSupportFragmentManager().beginTransaction()
+                                .setCustomAnimations(R.anim.fade_in, R.anim.fade_out, R.anim.fade_in, R.anim.fade_out)
+                                .replace( R.id.fragment_container, settingsFragment ).commit();
+                        break;
+                }
+
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId( R.id.brew );
+
     }
+
 }
