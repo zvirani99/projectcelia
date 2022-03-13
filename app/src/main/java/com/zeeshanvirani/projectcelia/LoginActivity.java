76b156d133b81d679469dd7f811775d9b6abb892
zeeshanvirani <zvirani99@gmail.com>
2022-03-13 01:16:46 -0600

    new primary color, login page temp backend implemented

--- app/src/main/java/com/zeeshanvirani/projectcelia/LoginActivity.java
+++ app/src/main/java/com/zeeshanvirani/projectcelia/LoginActivity.java
@@ -6,13 +6,25 @@ import androidx.preference.PreferenceManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
     private ImageButton back_btn;
     private Button login_btn;
+
+    private TextInputEditText email_textbox;
+    private TextInputEditText password_textbox;
+
+    private TextView invalid_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,11 @@ public class LoginActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
+        email_textbox = (TextInputEditText) findViewById(R.id.email_textbox);
+        password_textbox = (TextInputEditText) findViewById(R.id.password_textbox);
+
+        invalid_text = (TextView) findViewById(R.id.invalid_login_text);
+
         back_btn = (ImageButton) findViewById(R.id.back_button);
         back_btn.setOnClickListener(view -> {
             // Return to launch activity
@@ -30,12 +47,22 @@ public class LoginActivity extends AppCompatActivity {
 
         login_btn = (Button) findViewById(R.id.login_button);
         login_btn.setOnClickListener( view -> {
+            invalid_text.setVisibility( View.GONE );
             // Check if email and password are valid
-            // Set isloggedin to true
-            editor.putBoolean("isLoggedIn", true);
-            editor.apply();
-            // Transfer user to mainactivity
-            startActivity( new Intent(this, MainActivity.class) );
+            if ( Arrays.asList( TempDatabaseClass.email ).contains(email_textbox.getText().toString())
+                && Arrays.asList( TempDatabaseClass.passwords )
+                    .get( Arrays.asList( TempDatabaseClass.email ).indexOf(email_textbox.getText().toString()) )
+                    .equals( password_textbox.getText().toString()) ) {
+                // Set isloggedin to true
+                editor.putBoolean("isLoggedIn", true);
+                editor.apply();
+                // Transfer user to mainactivity
+                startActivity( new Intent(this, MainActivity.class) );
+            } else {
+                // Display that email or password is invalid
+                invalid_text.setVisibility( View.VISIBLE );
+            }
+
         });
 
 
--- app/src/main/java/com/zeeshanvirani/projectcelia/TempDatabaseClass.java
+++ app/src/main/java/com/zeeshanvirani/projectcelia/TempDatabaseClass.java
@@ -2,11 +2,18 @@ package com.zeeshanvirani.projectcelia;
 
 public class TempDatabaseClass {
 
-    // Testing Data : START
+    // Brewing Data : START
     public static String[] brewHistory_ids = new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8"};
     public static String[] brewHistory_dates = new String[]{"Dec 28, 2018", "Dec 29, 2018", "Dec 30, 2018", "Dec 28, 2018", "Dec 29, 2018", "Dec 30, 2018", "Dec 28, 2018", "Dec 29, 2018", "Dec 30, 2018"};
     public static String[] brewHistory_roasttypes = new String[]{"Light Roast", "Medium Roast", "Dark Roast", "Light Roast", "Medium Roast", "Dark Roast", "Light Roast", "Medium Roast", "Dark Roast"};
     public static String[] brewHistory_ratings = new String[]{"null", "7/10", "9/10", "6/10", "7/10", "9/10", "6/10", "7/10", "9/10"};
+    // END
+
+    // Account Data : START
+    public static String[] userid = new String[]{"1", "2"};
+    public static String[] email = new String[]{"admin@admin.com", "user@user.com"};
+    public static String[] passwords = new String[]{"admin", "user"};
+    public static String[] deviceID = new String[]{"1234", "5678"};
     // END
 
 }
