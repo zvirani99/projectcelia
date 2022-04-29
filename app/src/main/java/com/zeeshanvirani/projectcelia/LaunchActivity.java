b65bd747f548f325524d2ce2bda0362634d5beaf
zeeshanvirani <zvirani99@gmail.com>
2022-04-28 20:28:53 -0500

    added location services permission request

--- app/src/main/java/com/zeeshanvirani/projectcelia/LaunchActivity.java
+++ app/src/main/java/com/zeeshanvirani/projectcelia/LaunchActivity.java
@@ -1,7 +1,10 @@
 package com.zeeshanvirani.projectcelia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
@@ -22,6 +25,12 @@ public class LaunchActivity extends AppCompatActivity {
         // Create notification channel
         DataHandler.createNotificationChannel( this );
 
+        Button createaccount_btn = findViewById(R.id.button_createaccount);
+        createaccount_btn.setOnClickListener(view -> {
+            // Switch to Create Account Activity so user can create their new account
+            startActivity( new Intent(this, CreateAccountActivity.class) );
+        });
+
         // Check if user is already logged in
         if ( FirebaseAuth.getInstance().getCurrentUser() != null ) {
             // User is logged in
@@ -29,12 +38,6 @@ public class LaunchActivity extends AppCompatActivity {
             startActivity(new Intent(this, MainActivity.class));
             finish();
         }
-
-        Button createaccount_btn = findViewById(R.id.button_createaccount);
-        createaccount_btn.setOnClickListener(view -> {
-            // Switch to Create Account Activity so user can create their new account
-            startActivity( new Intent(this, CreateAccountActivity.class) );
-        });
 
         Button login_btn = findViewById(R.id.button_login);
         login_btn.setOnClickListener(view -> {
@@ -54,6 +57,21 @@ public class LaunchActivity extends AppCompatActivity {
             createaccount_btn.setEnabled( true );
             login_btn.setEnabled( true );
         }
+
+        // Check if bluetooth is available
+        if ( BluetoothAdapter.getDefaultAdapter() == null ) {
+            Toast.makeText(getApplicationContext(), "Bluetooth is not available on this device. Application cannot function without bluetooth capabilities.",
+                    Toast.LENGTH_SHORT).show();
+            createaccount_btn.setEnabled( false );
+            login_btn.setEnabled( false );
+        } else {
+            createaccount_btn.setEnabled( true );
+            login_btn.setEnabled( true );
+        }
+
+        // Request Location Permission
+        int CONSTANT = 0;
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, CONSTANT);
 
     }
 }
